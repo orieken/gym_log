9ce8e9b32c054e77c543863ae9a9641ad3ded9b1
Oscar Rieken <oriekenjr@gmail.com>
2012-08-11 14:05:37 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gymrat::Application.config.secret_token = 'a23781d0d7f24cd4d987134e455d361f7219913fdd3bf18f32ae000e62e3177263d32aa9b91f17dc88933a3d77a610be79144ded76606848484829a63fb66e99'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Gymrat::Application.routes.draw do
+  root :to => "home#index"
+end
