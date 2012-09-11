d8dcd99164a36ce24d2a5638a8c689e0b772db70
William Poindexter <wpoindexter@bimfizzle.com>
2012-09-10 23:20:37 -0400

    I remember that there were 4 main exercises, but I don't remember what they were supposed to be.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 Exercise.create(name: 'Bench Press', main: true)
 Exercise.create(name: 'Squat', main: true)
+Exercise.create(name: 'Some Roid Raging Thing', main: true)
+Exercise.create(name: 'The Clear', main: true)
